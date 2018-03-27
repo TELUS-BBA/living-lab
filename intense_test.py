c5e152e013c9f93ea95834e88ea41b16899fdf8e
Adam Pickering <adam@apu2-adam.telus>
2018-03-27 14:57:24 -0600

    Renamed new_client.py to intense_test.py

--- intense_test.py
+++ intense_test.py
@@ -2,6 +2,7 @@
 
 import iperf3
 import socket
+import time
 
 
 HOST = "192.168.1.101"
@@ -13,11 +14,7 @@ def test_up(host, port):
     test_client.server_hostname = host
     test_client.num_streams = 4
     result = do_iperf_test(host, port, test_client)
-    try:
-        return result.sent_Mbps
-    except:
-        print(result)
-        raise
+    return result.sent_Mbps
 
 
 def test_down(host, port):
@@ -44,15 +41,21 @@ def do_iperf_test(host, port, iperf_test):
         print("sending SENDPORT")
         s.sendall("SENDPORT\r\n".encode())
         iperf_port = int(s.recv(4096).decode())
-        print("port received: {}".format(iperf_port))
-        print("type of iperf_port: {}".format(type(iperf_port)))
         print("running test")
         iperf_test.port = iperf_port
+        time.sleep(1)
         result = iperf_test.run()
     return result
 
 
 if __name__ == "__main__":
-    print("Up: {} Mbit/s".format(test_up(HOST, PORT)))
-    #print("Down: {} Mbit/s".format(test_down(HOST, PORT)))
-    #print("Jitter: {} ms".format(test_jitter(HOST, PORT)))
+
+    # do tests
+    up_result = test_up(HOST, PORT)
+    print("up result: {}".format(up_result))
+    down_result = test_down(HOST, PORT)
+    print("down result: {}".format(down_result))
+    jitter_result = test_jitter(HOST, PORT)
+    print("jitter result: {}".format(jitter_result))
+
+    # send data to API (to be completed)
