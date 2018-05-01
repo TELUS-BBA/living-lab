060c40d6b27ce8696b764407493b0ba0699661b5
Adam Pickering <adamkpickering@gmail.com>
2018-05-01 18:38:35 +0000

    Updated provision_nanopi.py; should now be good

--- files/provision_nanopi.py
+++ files/provision_nanopi.py
@@ -7,8 +7,6 @@ import requests
 from requests.auth import HTTPBasicAuth
 import json
 
-API_HOST = "http://192.168.1.101:8000/provisioning/nanopi/"
-
 parser = argparse.ArgumentParser()
 parser.add_argument("user")
 parser.add_argument("password")
@@ -20,7 +18,7 @@ API_HOST = "http://{}:{}/provisioning/nanopi/".format(args.hostname, args.port)
 
 # get MAC and parse into a string without colons (to be used as username)
 mac = open('/sys/class/net/'+'eth0'+'/address').readline()
-username = mac.replace(":", "")
+username = mac.replace(":", "").rstrip()
 
 # generate random password that can still be typed by a human
 wordfile = xp.locate_wordfile()
@@ -31,13 +29,22 @@ password =  xp.generate_xkcdpassword(mywords, numwords=4).replace(" ", "")
 #admin_username = input("API Admin Username: ")
 #admin_password = getpass(prompt="API Admin Password: ")
 
-# post the username and password to API
-response = requests.post(API_HOST, data={'username': username, 'password': password},
-                         auth=HTTPBasicAuth(args.user, args.password))
+# update/create the API record of the nanopi
+get_response_json = requests.get(API_HOST, auth=HTTPBasicAuth(args.user, args.password)).json()
+same_username = [nanopi for nanopi in get_response_json if (nanopi.get('username') == username)]
+if len(same_username) > 0:
+    response = requests.put(API_HOST+'{}/'.format(same_username[0].get('id')),
+                            data={'username': username, 'password': password},
+                            auth=HTTPBasicAuth(args.user, args.password))
+else:
+    response = requests.post(API_HOST, data={'username': username, 'password': password},
+                             auth=HTTPBasicAuth(args.user, args.password))
 response.raise_for_status()
-print("Success: provisioned NanoPi with username {} and password {}".format(username, password))
+response_json = response.json()
 
 # write results to file
-with open('/home/nanopi/info') as fd:
-    info = {"username": username, "password": password, "port": port}
-    fd.write(json.dumps(info))
+with open('/home/nanopi/info', 'w+') as fd:
+    #info = {"username": username, "password": password, "port": response_json.get("ssh_port")}
+    fd.write(json.dumps(response_json))
+
+print(response_json.get('ssh_port'))
